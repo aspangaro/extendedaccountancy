f67c7930e4f748349215e7d8a0ca667328d63c41
Florian HENRY <florian.henry@open-concept.pro>
2021-05-31 08:35:22 +0200

    better SQL file

---
+++ sql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO llx_c_type_transaction (code, label, active) values ('STANDARD', 'Standard', 1);
+INSERT INTO llx_c_type_transaction (code, label, active) values ('LOCATION', 'Location', 0);
+
+
---
+++ sql/llx_c_type_transaction.key.sql
@@ -0,0 +1,2 @@
+ALTER TABLE llx_c_type_transaction ADD UNIQUE INDEX uk_c_type_transaction(code);
+
